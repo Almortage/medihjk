e142a4d7a89480c855e2ab4d5c6650697005fdcf
sa3ed7asan <127165039+sa3ed7asan@users.noreply.github.com>
2023-10-13 01:00:55 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from threading import Thread
+
+s = Flask('')
+
+@s.route('/')
+def ping():
+  return "Pong!"
+
+def run():
+  s.run(host='0.0.0.0', port=8000)
+  
+def server():
+  t = Thread(target=run)
+  t.start()
